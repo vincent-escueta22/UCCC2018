14bc9c7218b8dbf9ec490da39d3aa2e3dd923925
vincent <vincent.escueta22@berkeley.edu>
2017-10-04 14:54:09 -0700

    lets try the real website

--- js/header.js
+++ js/header.js
@@ -1,10 +1,13 @@
 function dontmove() {
+	var image_x = document.getElementById('bg');
+	image_x.parentNode.removeChild(image_x);
+	// alert("i did stuff");
 	// alert($("header").css('background'));
-    $("header").css('background', 'url("../img/background.png") no-repeat center center'
+    // $("header").css('background', 'url("../img/background.png") no-repeat center center'
     // $("header").css('background', 'url("../img/background.png") no-repeat scroll 50% 50% / auto padding-box border-box');
-    $("header").css('-webkit-background-size', 'cover');
-    $("header").css('-moz-background-size', 'cover');
-    $("header").css('background-size', 'cover');
-    $("header").css('-o-background-size', 'cover');
+    // $("header").css('-webkit-background-size', 'cover');
+    // $("header").css('-moz-background-size', 'cover');
+    // $("header").css('background-size', 'cover');
+    // $("header").css('-o-background-size', 'cover');
     // alert($("header").css('background'));
 }
